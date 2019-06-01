22b960b37dd636429252d5e55497428f1f3d1ee8
akuksov <kuksoff.alexey@gmail.com>
2019-06-01 16:21:48 +0300

    pyowm off

--- tgbot.py
+++ tgbot.py
@@ -3,11 +3,11 @@
 # import socks
 # import socket
 import telebot
-import pyowm
+#import pyowm
 
 # owm = pyowm.OWM('ee53bd221ce171abd050ae88362dc095', language="ru")
 TOKEN = '823149895:AAHOU6KMCsjF-swa7fKLFyVUZpUoQvREy8U'
-owm = pyowm.OWM('ee53bd221ce171abd050ae88362dc095', language="ru")
+#owm = pyowm.OWM('ee53bd221ce171abd050ae88362dc095', language="ru")
 bot = telebot.TeleBot(TOKEN)
 
 
@@ -17,14 +17,14 @@ def start_handler(message):
         message.chat.id, 'Привет, народ!')
 
 
-@bot.message_handler(content_types=['text'])
-def send_welcome(message):
-    observation = owm.weather_at_place(message.text)
-    w = observation.get_weather()
-    temp = w.get_temperature('celsius')['temp']
-    answer = 'В городе ' + message.text + ' сейчас ' + str(temp)
-    bot.send_message(message.chat.id, answer)
-    bot.send_message(message.chat.id, w)
+# @bot.message_handler(content_types=['text'])
+# def send_welcome(message):
+#     observation = owm.weather_at_place(message.text)
+#     w = observation.get_weather()
+#     temp = w.get_temperature('celsius')['temp']
+#     answer = 'В городе ' + message.text + ' сейчас ' + str(temp)
+#     bot.send_message(message.chat.id, answer)
+#     bot.send_message(message.chat.id, w)
 
 
 bot.polling(none_stop=True)
