c630837521f9c822eee8d93775cbf1a6e276077a
akuksov <kuksoff.alexey@gmail.com>
2019-06-01 23:54:54 +0300

    pywom 2.8.0

--- tgbot.py
+++ tgbot.py
@@ -7,6 +7,7 @@ owm = pyowm.OWM('ee53bd221ce171abd050ae88362dc095')
 bot = telebot.TeleBot(TOKEN)
 trans_detailed = {'clear sky': 'Ясно',
                   'few clouds': 'Переменная облачность',
+                  'overcast clouds': 'Облачно',
                   'scattered clouds': 'Облачно',
                   'broken clouds': 'Тучи',
                   'shower rain': 'Ливень',
@@ -16,30 +17,33 @@ trans_detailed = {'clear sky': 'Ясно',
                   'mist': 'Дымка',
                   }
 
+err_mesage = 'Вводи нормально, дуралей!'
+
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
     bot.send_message(
-        message.chat.id, 'Привет!')
+        message.chat.id, 'Привет!\n\nНапишите название города, погоду в котором вы хотели бы узнать.')
 
 
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
-    observation = owm.weather_at_place(message.text)
-    w = observation.get_weather()
+    try:
+        observation = owm.weather_at_place(message.text)
+        w = observation.get_weather()
+        answer = 'Сегодня в ' + message.text + ':\n\n' + \
+                 str(trans_detailed[(w.get_detailed_status())]) + \
+                 '\nТемпература воздуха - ' + str(w.get_temperature('celsius')['temp']) + '°' + \
+                 '\nОблачность - ' + str(w.get_clouds()) + \
+                 '\nВетер ' + str(w.get_wind()['speed']) + ' м/с' + \
+                 '\nНаправление - ' + str(w.get_wind()['deg']) + '°' + \
+                 '\nАтмосферное давление - ' + str("{0:.0f}".format(round(w.get_pressure()['press'] / 1.333,
+                                                                          0))) + ' мм.рт.ст' + \
+                 '\nОтносительная влажность - ' + str(w.get_humidity()) + '%'
 
-    # answer = 'Сегодня в ' + message.text + w.get_status + ' сейчас ' + str(temp)
-    answer = 'Сегодня в ' + message.text + ':\n\n' + \
-             str(trans_detailed[(w.get_detailed_status())]) + \
-             '\nТемпература воздуха - ' + str(w.get_temperature('celsius')['temp']) + '°' + \
-             '\nОблачность - ' + str(w.get_clouds()) + \
-             '\nВетер ' + str(w.get_wind()['speed']) + ' м/с' + \
-             '\nНаправление - ' + str(w.get_wind()['deg']) + '°' + \
-             '\nАтмосферное давление - ' + str("{0:.0f}".format(round(w.get_pressure()['press'] / 1.333,
-                                                                      0))) + ' мм.рт.ст' + \
-             '\nОтносительная влажность - ' + str(w.get_humidity()) + '%'
-
-    bot.send_message(message.chat.id, answer)
+        bot.send_message(message.chat.id, answer)
+    except:
+        bot.send_message(message.chat.id, err_mesage)
 
 
 bot.polling(none_stop=True)
