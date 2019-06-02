08c4fba65d080be7de322a82e28aae1e0d500432
akuksov <kuksoff.alexey@gmail.com>
2019-06-03 00:12:17 +0300

    5 day forecast

--- tgbot.py
+++ tgbot.py
@@ -2,7 +2,6 @@ import telebot
 import pyowm
 from datetime import datetime, timedelta
 
-
 TOKEN = '823149895:AAGUwtRQ9dOQPvtqA8ZxZYmhd2MA4GbUK8k'
 owm = pyowm.OWM('ee53bd221ce171abd050ae88362dc095')
 bot = telebot.TeleBot(TOKEN)
@@ -24,7 +23,6 @@ weather_1h = datetime.now() + timedelta(hours=1)
 weather_3h = datetime.now() + timedelta(hours=3)
 weather_5h = datetime.now() + timedelta(hours=5)
 
-
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
     bot.send_message(
@@ -38,6 +36,14 @@ def send_welcome(message):
         f_3h = owm.three_hours_forecast(message.text)
         w = observation.get_weather()
         w2 = f_3h.get_forecast()
+        a_forecast = ''
+        forecast = ''
+        for weather in w2:
+            f_date = str('{:%d %b - %H:%M}'.format((weather.get_reference_time('date') + timedelta(hours=3))))
+            f_temp = str(weather.get_temperature('celsius')['temp'])
+            f_status_detailed = str(weather.get_detailed_status())
+            forecast = f_date + ' ' + f_temp + ' ' + f_status_detailed + '\n'
+            a_forecast = (a_forecast) + str(forecast)
         answer = 'Сегодня в ' + message.text + ':\n\n' + \
                  str(trans_detailed[(w.get_detailed_status())]) + \
                  '\nТемпература воздуха - ' + str(w.get_temperature('celsius')['temp']) + '°' + \
@@ -46,7 +52,11 @@ def send_welcome(message):
                  '\nНаправление - ' + str(w.get_wind()['deg']) + '°' + \
                  '\nАтмосферное давление - ' + str("{0:.0f}".format(round(w.get_pressure()['press'] / 1.333,
                                                                           0))) + ' мм.рт.ст' + \
-                 '\nОтносительная влажность - ' + str(w.get_humidity()) + '%\n\n\n' + 'Прогноз на 3 дня:\n'
+                 '\nОтносительная влажность - ' + str(w.get_humidity()) + '%\n\n\n' + 'Прогноз на 5 дней:\n' + str(a_forecast)
+
+
+
+
         bot.send_message(message.chat.id, answer)
         # forecast = str(w2.get_weathers())
         # bot.send_message(message.chat.id, forecast)
