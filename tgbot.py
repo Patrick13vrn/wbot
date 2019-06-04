cb56c5151f2b7bf13eafc35c8433f2775d8734da
akuksov <kuksoff.alexey@gmail.com>
2019-06-04 07:17:19 +0300

    condition fix

--- tgbot.py
+++ tgbot.py
@@ -55,30 +55,18 @@ def send_welcome(message):
         w = observation.get_weather()
         w2 = f_3h.get_forecast()
         a_forecast = ''
-        # forecast = ''
+        forecast = ''
+        answer = str(owm.is_API_online())
         for weather in w2:
             f_date = str('{:%d %b - %H:%M}'.format((weather.get_reference_time('date') + timedelta(hours=3))))
             f_temp = str(weather.get_temperature('celsius')['temp'])
-            # f_rain = str(weather.get_rain())
             f_status_detailed = str(weather.get_detailed_status())
             forecast = f_date + ' ' + f_temp + ' ' + f_status_detailed + ' ' + '\n'
             a_forecast = a_forecast + str(forecast)
-            if '{:%d %b - %H:%M}'.format(
-                    weather.get_reference_time('date') + timedelta(hours=3)) <= '{:%d %b - %H:%M}'.format(
-                    (datetime.now())):
-                # str(trans_detailed[(w.get_detailed_status())]) + \
-                answer = 'Сегодня в ' + message.text + ':\n\n' + str((w.get_detailed_status())) + \
-                         '\nТемпература воздуха - ' + str(w.get_temperature('celsius')['temp']) + '°' + \
-                         '\nОблачность - ' + str(w.get_clouds()) + '%' + \
-                         '\nВетер ' + str(w.get_wind()['speed']) + ' м/с, ' + wind_d(w.get_wind()['deg']) + \
-                         '\nАтмосферное давление - ' + str("{0:.0f}".format(round(w.get_pressure()['press'] / 1.333,
-                                                                                  0))) + ' мм.рт.ст' + \
-                         '\nОтносительная влажность - ' + str(w.get_humidity()) + '%\n' + 'Обновление ' + str(
-                    '{:%d.%m.%y %H:%M:%S}'.format(
-                        w.get_reference_time(timeformat='date') + timedelta(
-                            hours=3))) + '\n\n\n' + 'Прогноз на 3 дня:\n' + str(a_forecast)
-            else:
-                break
+            # if '{:%d %b - %H:%M}'.format(weather.get_reference_time('date') + timedelta(hours=3)) <= '{:%d %b - %H:%M}'.format((datetime.now())):
+            answer = 'Сегодня в ' + message.text + ':\n\n' + str((w.get_detailed_status())) + '\nТемпература воздуха - ' + str(w.get_temperature('celsius')['temp']) + '°' + '\nОблачность - ' + str(w.get_clouds()) + '%' + '\nВетер ' + str(w.get_wind()['speed']) + ' м/с, ' + wind_d(w.get_wind()['deg']) + '\nАтмосферное давление - ' + str("{0:.0f}".format(round(w.get_pressure()['press'] / 1.333, 0))) + ' мм.рт.ст' + '\nОтносительная влажность - ' + str(w.get_humidity()) + '%\n' + 'Обновление ' + str('{:%d.%m.%y %H:%M:%S}'.format(w.get_reference_time(timeformat='date') + timedelta(hours=3))) + '\n\n\n' + 'Прогноз на 3 дня:\n' + str(a_forecast)
+            # else:
+            #     break
         bot.send_message(message.chat.id, answer)
         # forecast = str(w2.get_weathers())
         # bot.send_message(message.chat.id, forecast)
