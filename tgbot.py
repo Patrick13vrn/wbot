54e205439aa95ead38365f98d5862eb4bab094a7
akuksov <kuksoff.alexey@gmail.com>
2019-06-12 01:06:00 +0300

    emoji support

--- tgbot.py
+++ tgbot.py
@@ -24,6 +24,73 @@ def wind_d(direction):
     }[True]
 
 
+def temp(temp):
+    temp2 = float(temp)
+    if temp2 < 0:
+        temp_sign = '-'
+    elif temp2 > 0:
+        temp_sign = '+'
+    else:
+        temp_sign = ''
+    temp_result = temp_sign + str(round(temp2))
+    return temp_result
+
+
+emojies = {
+    200: "\u26c8",
+    201: "\u26c8",
+    202: "\u26c8",
+    210: "\ud83c\udf29",
+    211: "\u26c8",
+    212: "\ud83c\udf2a",
+    221: "\u26c8",
+    230: "\u26c8",
+    231: "\u26c8",
+    232: "\u26c8",
+    300: "\ud83d\udca7",
+    301: "\ud83d\udca7",
+    302: "\ud83d\udca6",
+    310: "\ud83c\udf26",
+    311: "\ud83c\udf26",
+    312: "\ud83c\udf27",
+    313: "\ud83c\udf28",
+    314: "\ud83c\udf27",
+    321: "\ud83c\udf26",
+    500: "\ud83c\udf26",
+    501: "\ud83c\udf28",
+    502: "\ud83c\udf27",
+    503: "\ud83c\udf27",
+    504: "\ud83c\udf27",
+    511: "\ud83c\udf27",
+    520: "\ud83c\udf28",
+    521: "\ud83c\udf28",
+    522: "\ud83c\udf27",
+    531: "\ud83c\udf26",
+    600: "\u2744\ufe0f",
+    601: "\u2744\ufe0f",
+    602: "\u2744\ufe0f",
+    611: "\ud83d\udca7",
+    612: "\u2744\ufe0f",
+    620: "\u2744\ufe0f",
+    621: "\u2744\ufe0f",
+    622: "\u2744\ufe0f",
+    701: "\ud83c\udf2b",
+    711: "\ud83c\udf2b",
+    721: "\ud83c\udf2b",
+    731: "\ud83d\udca8",
+    741: "\ud83c\udf2b",
+    751: "\ud83d\udca8",
+    761: "\ud83d\udca8",
+    762: "\ud83d\udca8",
+    771: "\ud83d\udca8",
+    781: "\ud83c\udf2a",
+    800: "\u2600\ufe0f",
+    801: "\u2601\ufe0f",
+    802: "\ud83c\udf24",
+    803: "\u2601\ufe0f",
+    804: "\u2601\ufe0f"
+}
+
 err_message = 'Что-то пошло не так...'
 
 
@@ -51,6 +118,9 @@ def send_welcome(message):
         w_wspeed = w.get_wind()['speed']
         w_temp = w.get_temperature('celsius')['temp']
         w_press = w.get_pressure()['press']
+        w_code = w.get_weather_code()
+
+        # print(w2.get_weather)
 
         try:
             w_wdeg = (w.get_wind()['deg'])
@@ -106,9 +176,6 @@ def send_welcome(message):
                 forecast += str(datetime.strftime(f_date, '%H:%M')) + ' '
                 forecast += template
 
-
-
-
             # вывод прогноза на 3 дня
             if f_now3 < f_date3 <= f_end:
                 if lastrain != '0':
@@ -121,17 +188,15 @@ def send_welcome(message):
                     a_forecast += 'Ночью: ' + template
                 elif f_time == f_day:
                     a_forecast += 'Днём: ' + template + '\n'
-        emoji = b'\xE2\x9B\x85'.decode('utf-8')
-        answer = '*Сейчас в ' + texts + ':*\n\n' + emoji + str(w_det).title() + \
-                 '\nТемпература воздуха - ' + str(w_temp) + '°' + \
-                 '\nОблачность - ' + str(w_cloud) + '%' + \
-                 '\nВетер ' + str(w_wspeed) + ' м/с, ' + wind_d(w_wdeg) + \
-                 '\nАтмосферное давление - ' + str("{0:.0f}".format(round(w_press / 1.333, 0))) + ' мм.рт.ст' + \
-                 '\nОтносительная влажность - ' + str(w_humid) + '%' + \
-                 '\n_Обновление ' + str('{:%d.%m.%y %H:%M:%S}'.format(w_rec_time)) + '_\n\n' + str(forecast) + \
+        answer = '*Сейчас в ' + texts + ':*\n\n' + (emojies.get(w_code, '')) + str(w_det).title() + \
+                 '\nТемпература воздуха: ' + temp(w_temp) + '°' + \
+                 '\nОблачность: ' + str(w_cloud) + '%' + \
+                 '\nВетер: ' + str(w_wspeed) + ' м/с, ' + wind_d(w_wdeg) + \
+                 '\nАтмосферное давление: ' + str("{0:.0f}".format(round(w_press / 1.333, 0))) + ' мм.рт.ст' + \
+                 '\nОтносительная влажность: ' + str(w_humid) + '%' + \
+                 '\n_Обновление от' + str('{:%d.%m.%y %H:%M:%S}'.format(w_rec_time)) + '_\n\n' + str(forecast) + \
                  '\n' + 'Прогноз на 3 дня:\n\n' + str(a_forecast)
         bot.send_message(message.chat.id, answer, parse_mode='Markdown')
-
 
     except Exception as e:
         bot.send_message(message.chat.id, e)
