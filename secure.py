2d8a5eecf683a7af0d84c05f51b07eedbd8c5e92
akuksov <kuksoff.alexey@gmail.com>
2019-06-23 23:15:45 +0300

    Revert "security"
    This reverts commit 8f228a23

---
+++ secure.py
@@ -0,0 +1,9 @@
+def tg_token():
+    token = '823149895:AAGUwtRQ9dOQPvtqA8ZxZYmhd2MA4GbUK8k'
+    return token
+
+
+def owm_token():
+    token = 'ee53bd221ce171abd050ae88362dc095'
+    #dsfs
+    return token
